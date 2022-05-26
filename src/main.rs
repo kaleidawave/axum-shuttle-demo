8e4f162cad8dafa0133f0f9ffacda836b822623e
Ben <kaleidawave@gmail.com>
2022-05-26 12:12:58 +0100

    Testing shuttle-deploy-action

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use axum;
-
 use axum_shuttle_demo;
 
 const MERRIAM_WEBSTER_API_KEY: Option<&str> = option_env!("MERRIAM_WEBSTER_API_KEY");
@@ -7,7 +6,6 @@ const MERRIAM_WEBSTER_API_KEY: Option<&str> = option_env!("MERRIAM_WEBSTER_API_K
 #[tokio::main]
 async fn main() {
     println!("Spinning server up on http://localhost:3000");
-    // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(axum_shuttle_demo::get_router(MERRIAM_WEBSTER_API_KEY).into_make_service())
         .await
